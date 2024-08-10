47d1c8d6f7c8766bfd136810d0b7ad423cfc1d77
Mudassir Zeya <mudassirzeya206@gmail.com>
2024-08-10 18:17:10 +0530

    attendace app with location & image 2

--- attendance/settings.py
+++ attendance/settings.py
@@ -131,5 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'techbodycraftmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'khgvcyjmfxlbcnga'
+EMAIL_HOST_USER = ''  # Enter Your Email ID
+
+# THis is not your Email ID Password You can Get this from "Mange Your Account" Section of your Gmail.
+EMAIL_HOST_PASSWORD = ''
